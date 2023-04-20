e0795a9008ba8fe4ebc344508804fe97ec68a932
ShukuratBello <belloshukurat03@gmail.com>
2023-04-20 23:48:06 +0000

    Add

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point
+ * main - Prints out the last part of a quote in the standard error.
  *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful- Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
