79a3f696597ae60548b0810080bfb7f6455dc8ff
ShukuratBello <belloshukurat03@gmail.com>
2023-04-20 23:52:47 +0000

    Add

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistdi.h>
 
 /**
  * main - Prints out the last part of a quote in the standard error.
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful- Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
