4554bf696cf24ab0fb023ab2c6d41620e65787d3
ShukuratBello <belloshukurat03@gmail.com>
2023-05-24 14:00:02 +0000

    0x0F-function_pointers Corrections 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,7 +7,7 @@
  * @f: function to print name
  * Return: Nothing
  */
-void print_name(char *name, void (*f)(char *name))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
