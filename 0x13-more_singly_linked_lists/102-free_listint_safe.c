1afb1077598d4d4a24cbff7a737bb25c45fef3b5
ShukuratBello <belloshukurat03@gmail.com>
2023-06-07 01:28:50 +0000

    more corrections

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - f function that frees a linked list
- * @h- A pointer
- * Return: The list of the list that was free
+ * free_listint_safe - A function that frees a list
+ * @h: A pointer listint_t structure
+ * Return: The size of the list that was free'd
  */
 size_t free_listint_safe(listint_t **h)
 {
