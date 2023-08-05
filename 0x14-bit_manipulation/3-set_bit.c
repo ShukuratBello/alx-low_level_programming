72a7898ed18a28bcd7e05728f446a5b6f7246022
ShukuratBello <belloshukurat03@gmail.com>
2023-08-05 00:21:48 +0000

    Task 2-5

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,11 +10,8 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int max = 0x01;
-
-	max <<= index;
-	if (max == 0)
+	if (index > (8 * sizeof(*n)) - 1)
 		return (-1);
-	*n |= max;
+	*n = *n | (1 << index);
 	return (1);
 }
